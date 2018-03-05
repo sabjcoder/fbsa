0763dc4a0f9a855d09bf5a3591eb7139ca199b6c
sabjcoder <35679634+sabjcoder@users.noreply.github.com>
2018-03-05 14:24:05 +0530

    Update fb_config.php

--- fb_config.php
+++ fb_config.php
@@ -1,9 +1,9 @@
 <?php
 require_once __DIR__ . '/vendor/autoload.php';
-$appId = '131460884347524';
+$appId = '2046848278920294';
 $fb = new Facebook\Facebook([
 	'app_id' => $appId,
-	'app_secret' => 'c3d73d017b978bc466b6329838ccfbf7',
+	'app_secret' => '5cd90ef0db943a0d1c3098be2a164797',
 	'default_graph_version' => 'v2.12',
 ]);
 ?>
