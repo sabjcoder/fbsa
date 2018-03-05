158dc25e237c39f7ea25c8a3c0791d476750e69d
sabjcoder <35679634+sabjcoder@users.noreply.github.com>
2018-03-05 14:24:39 +0530

    Update logout.php

--- logout.php
+++ logout.php
@@ -2,7 +2,7 @@
 include 'fb_config.php';
 	session_start();
 	$helper = $fb->getRedirectLoginHelper();
-	$fbLogoutUrl = $helper->getLogoutUrl($_SESSION['fb_access_token'], 'http://localhost:81/fb_sentiment/index.php');
+	$fbLogoutUrl = $helper->getLogoutUrl($_SESSION['fb_access_token'], 'https://fbsa-sentiment.herokuapp.com/index.php');
 	session_destroy();
 	header("location: $fbLogoutUrl");
 	exit;
