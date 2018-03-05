e6968454b5277cf259eca9402bda0edcbb2d0a18
sabjcoder <35679634+sabjcoder@users.noreply.github.com>
2018-03-05 14:09:16 +0530

    Delete fb_config.php

--- fb_config.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once __DIR__ . '/vendor/autoload.php';
-$appId = '131460884347524';
-$fb = new Facebook\Facebook([
-	'app_id' => $appId,
-	'app_secret' => '2b1786c5c413be56531c8b613898942a',
-	'default_graph_version' => 'v2.5',
-]);
-?>
